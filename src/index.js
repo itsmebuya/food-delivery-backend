ace58a1b16115d48f32a6e47414a83a2cbd79352
24LP7490 24LP7490 <itsmebuya@gmail.com>
2025-02-19 19:00:51 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express()
+const port = 3000; 
+app.use(express.json())
+
+
+
+
+app.listen(port, () => {
+    console.log(`example app listening on port ${port}`)
+})
