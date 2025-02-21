fbf96fc060015ef9078a910fbf41c0c5648167d2
24LP7490 24LP7490 <itsmebuya@gmail.com>
2025-02-21 18:01:28 +0800

    setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 // import { userRouter } from './routers/user.routes.js';
 import { userRouter } from './routers/User.routes.js';
+import cors from "cors"
 
 
 const url = "mongodb+srv://itsmebuya:iz6sl4GPugIQBJX3@cluster0.42tlq.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
@@ -15,13 +16,18 @@ const connectDB = async () => {
         console.log("Error occured ", error);
     }
 }
-connectDB();
+// connectDB();
 
 const app = express()
-const port = 3000;
+const port = 999;
 app.use(express.json())
+app.use(cors())
+// app.use("/users", userRouter )
 
-app.use("/users", userRouter )
+app.use("/", (req, res) => {
+    res.send("this is test")
+})
+
 
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`)
