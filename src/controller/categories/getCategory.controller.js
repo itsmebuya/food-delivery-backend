b18184eaf1caa7484d96f8009d21a89e41c4a8a7
24LP7490 24LP7490 <itsmebuya@gmail.com>
2025-03-28 18:40:12 +0800

    28

--- src/controller/categories/getCategory.controller.js
+++ src/controller/categories/getCategory.controller.js
@@ -2,7 +2,22 @@ import { Categories } from "../../models/categories.model.js";
 
 export const getCategory = async (req, res) => {
     try {
-        const categoryData = await Categories.find({});
+        const categoryData = await Categories.aggregate([
+           { $lookup: {
+                from: "foods", // The collection to join with
+                localField: "_id", // The field in the current collection
+                foreignField: "category", // The field in the other collection
+                as: "foodCount" // The name of the array field to store the joined documents
+              }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    categoryName:1,
+                    foodCount: { $size:"$foodCount" }
+                }
+            }
+        ]);
         res.send(categoryData).status(200)
     } catch (error) {
         res.send().status(400)
